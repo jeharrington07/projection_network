7496d024e4cd1da833a7b17665327937e0829134
mariaannae <maria.edwards@gmail.com>
2021-07-14 22:54:27 -0400

    adding control over dropout weights

--- evol_search.py
+++ evol_search.py
@@ -1,12 +1,14 @@
 import numpy as np
+import torch as torch
 
 from ribs.archives import GridArchive
-from ribs.emitters import ImprovementEmitter
+from ribs.emitters import ImprovementEmitter, OptimizingEmitter
 from ribs.optimizers import Optimizer
 
 from random_network import create_random_dense, random_fake
 
-def fitness_func(x):
+def fitness_func(solution, projector):
+
     #dummy fitness function
     #TODO: replace with rastrigin funciton
     fitness = 1
@@ -14,6 +16,9 @@ def fitness_func(x):
     b2 = 1
     return fitness, b1, b2
 
+#TODO: Put the below inside a main function, set up command line args
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 #edit these fields according to the problem
 archiveSize = [10, 10]
@@ -22,16 +27,21 @@ yrange = (-1, 1)
 n_iter = 10
 n_features = 10
 
-
+#setting up elements for cma-es (for cma-me, use ImprovementEmitter)
 archive = GridArchive(archiveSize, [xrange, yrange])
-emitters = [ImprovementEmitter(archive, [0.0]*n_features, .1)]
+emitters = [OptimizingEmitter(archive, [0.0]*n_features, .1)]
 optimizer = Optimizer(archive, emitters)
 
-for itr in range(n_iter):
-    solutions = optimizer.ask()
+#initialize projection network
+#TODO: save weights
+projector = create_random_dense(100, 200, device, 3)
 
-    results = [fitness_func(solution) for solution in solutions]
-    objectives = [result[0] for result in results]
-    bcs = [result[1:] for result in results]
+with torch.no_grad():
 
-    optimizer.tell(objectives, bcs)
+    for itr in range(n_iter):
+        solutions = optimizer.ask()
+        results = [fitness_func(solution, projector) for solution in solutions]
+        objectives = [result[0] for result in results]
+        bcs = [result[1:] for result in results]
+
+        optimizer.tell(objectives, bcs)
--- random_network.py
+++ random_network.py
@@ -4,21 +4,24 @@ from sklearn.preprocessing import StandardScaler
 from torch.nn.modules.batchnorm import BatchNorm2d
 
 #TODO: experiment with network parameters. convolutional network (see https://github.com/openai/random-network-distillation, maybe follow the architecture of toad-gan for this part)
-def create_random_dense(input_size, output_size, device):
+
+#dropout of x initializes one weight in every x to 0
+def create_random_dense(input_size, output_size, device, dropout):
 
     class RandomNet(nn.Module):
         def __init__(self, device):
             super(RandomNet, self).__init__()
             self.device = device
-            self.main = nn.Sequential(
-                nn.Linear(in_features = input_size, out_features = output_size, bias = False),
-                nn.ReLU()
-            )
+            self.linear = nn.Linear(in_features = input_size, out_features = output_size, bias = False)
+            inds = [num for num in range(len(self.linear.weight.data[0])) if num % dropout == 0]
+            inds = torch.tensor(inds)
+            print(inds)
+            self.linear.weight.data.index_fill_(1, inds, 0)
+            print(self.linear.weight.data)
 
         def forward(self, input):
             #output = self.norm(self.main(input))
             #return output
-
             output = self.main(input)
             return output
     return RandomNet(device)
